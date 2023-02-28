30ed7bf1bfacb119eab1c3d10ae28fb7ad979b6e
Marcin Witas <77293179+Wojtazzzz@users.noreply.github.com>
2023-02-28 13:42:18 +0100

    feat/add more servers to seeder

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -4,43 +4,77 @@ import { z } from 'zod';
 
 const prisma = new PrismaClient();
 
-const serverSchema = z.object({
+const onlineServer = z.object({
+	online: z.literal(true),
 	ip: z.string(),
-	motd: z.object({
-		html: z.array(z.string()),
-	}),
+	hostname: z.string(),
+	port: z.number(),
+	motd: z
+		.object({
+			html: z.array(z.string()),
+		})
+		.optional(),
 	players: z.object({
 		online: z.number(),
 		max: z.number(),
 	}),
 	version: z.string(),
-	hostname: z.string(),
+
 	icon: z.string(),
 });
 
-const fetchServer = async () => {
+const offlineServer = z.object({
+	online: z.literal(false),
+	ip: z.string(),
+	hostname: z.string(),
+	port: z.number(),
+});
+
+const serverSchema = z.union([onlineServer, offlineServer]);
+
+const fetchServer = async (ip: string) => {
 	const response = await axios
-		.get(`https://api.mcsrvstat.us/2/hypixel.net`)
+		.get(`https://api.mcsrvstat.us/2/${ip}`)
 		.then((response) => response.data);
 
 	return serverSchema.parse(response);
 };
 
+const servers = [
+	'hypixel.net',
+	'craftmc.pl',
+	'mc.craftapple.pl',
+	'minefox.pl',
+	'mc.realcraft.pl',
+	'ekipa.pro',
+	'minecrafter.pl',
+	'przygodacraft.pl',
+	'dcrft.pl',
+	'legacymc.pl',
+	'unitedingeo.pl',
+];
+
 async function main() {
 	await prisma.server.deleteMany({});
 
-	const server = await fetchServer();
+	servers.forEach(async (ip) => {
+		const server = await fetchServer(ip);
 
-	await prisma.server.create({
-		data: {
-			ip: server.hostname,
-			online: server.players.online,
-			slots: server.players.max,
-			tags: ['pvp', 'minigame'],
-			version: server.version,
-			icon: server.icon,
-			motd: server.motd.html,
-		},
+		if (!server.online) {
+			return;
+		}
+
+		await prisma.server.create({
+			data: {
+				ip: server.hostname,
+				online: server.players.online,
+				slots: server.players.max,
+				tags: ['pvp', 'minigame'],
+				version: server.version,
+				icon: server.icon,
+				motd: server.motd?.html,
+			},
+		});
 	});
 }
 
