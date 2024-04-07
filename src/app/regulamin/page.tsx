2f90b32befc01086abc0c0702ccfd511d0bb7813
Wojtazzzz <marcin.witas72@gmail.com>
2024-04-07 20:06:27 +0200

    refactor/reinstall-whole-project

--- src/app/regulamin/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function StatutePage() {
-	return <span>STATUTE</span>;
-}
--- src/utils/truncate.ts
+++
@@ -1,7 +0,0 @@
-export const truncateString = (str: string, maxLength: number, appendString: string) => {
-	if (str.length > maxLength) {
-		return str.slice(0, maxLength) + appendString;
-	}
-
-	return str;
-};
