7a29967dda5793ea025ff27dfd3ea632ee8a214f
Wojtazzzz <marcin.witas72@gmail.com>
2024-04-16 18:20:50 +0200

    ci: add vercel cron config

--- src/app/api/servers/index/route.ts
+++ src/app/api/servers/index/route.ts
@@ -1,6 +1,6 @@
 import prisma from "@/prisma";
 
-export const dynamic = "force-dynamic"; // defaults to auto
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   const servers = await prisma.server.findMany();
