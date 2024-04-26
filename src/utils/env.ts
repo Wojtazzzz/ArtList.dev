c6c0154baa3964d8e38299bfb4e9f93f31a96d30
Wojtazzzz <marcin.witas72@gmail.com>
2024-04-26 18:57:22 +0200

    fix

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,13 +1,13 @@
 const parseEnv = (value: unknown, name: string) => {
-  if (!value || typeof value !== "string") {
+  if (!value) {
     throw new Error(`Missing env variable called "${name}"`);
   }
 
-  if (!value || typeof value !== "string") {
+  if (typeof value !== "string") {
     throw new Error(`Unknown value of env variable called "${name}"`);
   }
 
-  return String(value);
+  return value;
 };
 
 export const API_URL = parseEnv(
