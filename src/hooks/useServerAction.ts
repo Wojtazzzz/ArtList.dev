99ac074440f00acd678aaf16a21f534e8148281a
Wojtazzzz <marcin.witas72@gmail.com>
2024-04-26 18:45:18 +0200

    refactor: remove unused hook

--- src/hooks/useServerAction.ts
+++
@@ -1,9 +0,0 @@
-import { useFormState } from "react-dom";
-import { addServer, initialState } from "@/actions/addServer";
-import { useEffect } from "react";
-
-export const useServerAction = () => {
-  const [state, formAction] = useFormState(addServer, initialState);
-
-  useEffect(() => {}, []);
-};
