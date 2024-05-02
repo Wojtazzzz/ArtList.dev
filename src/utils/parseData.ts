2baaae28341eb44ca973a90e136613c8309026e2
Wojtazz <marcin.witas72@gmail.com>
2024-05-02 16:55:36 +0200

    build/fix build process

--- src/utils/parseData.ts
+++ src/utils/parseData.ts
@@ -1,7 +1,5 @@
 import { z } from "zod";
 
 export const parseData = <T extends z.ZodTypeAny>(data: unknown, schema: T) => {
-  console.log(schema);
-  // return schema.parse(data) as z.infer<T>;
-  return data as z.infer<T>;
+  return schema.parse(data) as z.infer<T>;
 };
