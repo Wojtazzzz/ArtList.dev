4ae8467942f56b3694b0c02f23ba7bcc0079609a
Wojtazz <marcin.witas72@gmail.com>
2024-05-10 21:34:28 +0200

    refactor/extract ArtList logo to individual component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Logo } from "@/components/Logo";
 
 const links = [
   {
@@ -25,23 +26,9 @@ export const Footer = () => {
         <div className="grid-1 grid gap-5 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
           <div>
             <p className="mb-4 flex items-center justify-center font-semibold md:justify-start">
-              <svg
-                className="mr-1 h-4 w-4 text-gray-800 dark:text-white md:mr-1.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M19 6c0 1.657-3.134 3-7 3S5 7.657 5 6m14 0c0-1.657-3.134-3-7-3S5 4.343 5 6m14 0v6M5 6v6m0 0c0 1.657 3.134 3 7 3s7-1.343 7-3M5 12v6c0 1.657 3.134 3 7 3s7-1.343 7-3v-6"
-                />
-              </svg>
+              <div className="mr-1 md:mr-1.5">
+                <Logo size="sm" />
+              </div>
               ArtList
             </p>
             <p>
--- src/components/navigations/root/RootNavigation.tsx
+++ src/components/navigations/root/RootNavigation.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { Container } from "@/components/ui/Container";
 import { AddServerDialog } from "@/components/navigations/root/AddServerDialog";
 import { ChangeTheme } from "@/components/ChangeTheme";
+import { Logo } from "@/components/Logo";
 
 export const RootNavigation = () => {
   return (
@@ -16,23 +17,7 @@ export const RootNavigation = () => {
         <div className="flex w-full flex-wrap items-center justify-between gap-y-4">
           <div>
             <Link href="/" className="flex items-center space-x-1.5 pr-1.5">
-              <svg
-                className="h-6 w-6 text-gray-800 dark:text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M19 6c0 1.657-3.134 3-7 3S5 7.657 5 6m14 0c0-1.657-3.134-3-7-3S5 4.343 5 6m14 0v6M5 6v6m0 0c0 1.657 3.134 3 7 3s7-1.343 7-3M5 12v6c0 1.657 3.134 3 7 3s7-1.343 7-3v-6"
-                />
-              </svg>
+              <Logo size="base" />
 
               <div className="self-center whitespace-nowrap text-2xl font-semibold">
                 <TypographyH1>ArtList</TypographyH1>
