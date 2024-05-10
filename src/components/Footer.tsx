c2da810123bffade51c5431d22b8f0cc4b4a791d
Wojtazz <marcin.witas72@gmail.com>
2024-05-10 21:30:14 +0200

    fix/remove needless comments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -75,19 +75,6 @@ export const Footer = () => {
             <p className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
               Kontakt
             </p>
-            {/*<p className="mb-4 flex items-center justify-center md:justify-start">*/}
-            {/*  <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">*/}
-            {/*    <svg*/}
-            {/*      xmlns="http://www.w3.org/2000/svg"*/}
-            {/*      viewBox="0 0 24 24"*/}
-            {/*      fill="currentColor"*/}
-            {/*    >*/}
-            {/*      <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />*/}
-            {/*      <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />*/}
-            {/*    </svg>*/}
-            {/*  </span>*/}
-            {/*  info@example.com*/}
-            {/*</p>*/}
             <Link
               href="https://github.com/Wojtazzzz/ArtList.dev"
               rel="noopener noreferrer"
