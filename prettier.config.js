4d41e72c8cad6664f277e21ab9467968ca30f32d
Wojtazz <marcin.witas72@gmail.com>
2024-06-01 18:26:32 +0200

    style/update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,11 @@
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindConfig: "./tailwind.config.ts",
+	plugins: ['prettier-plugin-tailwindcss'],
+	tailwindConfig: './tailwind.config.ts',
+	jsxSingleQuote: false,
+	arrowParens: 'always',
+	printWidth: 80,
+	useTabs: true,
+	tabWidth: 4,
+	trailingComma: 'es5',
+	singleQuote: true
 };
