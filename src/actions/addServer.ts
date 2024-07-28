75f94bdd72669e815ab9ac2d71c62ba1cc1060e1
Wojtazz <marcin.witas72@gmail.com>
2024-07-28 20:47:37 +0200

    debug

--- src/actions/addServer.ts
+++ src/actions/addServer.ts
@@ -25,7 +25,11 @@ export const addServer = async ({ name }: AddServerPayload) => {
 		}),
 	});
 
+	console.log('Response: ', response);
+
 	const data = await response.json();
+
+	console.log('Data: ', data);
 
 	if (!response.ok) {
 		return {
