64de328961941e4e9d5bed75f71a7c4badff73ab
Wojtazz <marcin.witas72@gmail.com>
2024-10-02 21:54:17 +0200

    chore: remove prisma and log-to-file packages

--- tests/utils/clearDatabase.ts
+++
@@ -1,5 +0,0 @@
-import { prisma } from '@/prisma';
-
-export const clearDatabase = async () => {
-	await prisma.server.deleteMany();
-};
--- tests/utils/fixtures.ts
+++ tests/utils/fixtures.ts
@@ -1,10 +1,10 @@
 import { test as base } from '@playwright/test';
-import { clearDatabase } from './clearDatabase';
+// import { clearDatabase } from './clearDatabase';
 import { clearNextCache } from './clearNextCache';
 
 export const test = base.extend({
 	page: async ({ page }, use) => {
-		await clearDatabase();
+		// await clearDatabase();
 		await clearNextCache();
 
 		await use(page);
