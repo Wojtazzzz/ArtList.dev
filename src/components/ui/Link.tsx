1050e8df4513d18f24c16642a6e86255b8fa91d5
Wojtazz <marcin.witas72@gmail.com>
2024-10-02 21:48:32 +0200

    perf: add routing prefetching

--- src/components/ui/Link.tsx
+++ src/components/ui/Link.tsx
@@ -1,9 +1,12 @@
+'use client';
+
 import NextLink, { type LinkProps as NextLinkProps } from 'next/link';
-import type { AnchorHTMLAttributes } from 'react';
+import { useRouter } from 'next/navigation';
+import type { AnchorHTMLAttributes, ComponentPropsWithRef } from 'react';
 
 type LinkVariant = 'internal' | 'external';
 
-type LinksProps = {
+type LinksProps = ComponentPropsWithRef<typeof NextLink> & {
 	variant?: LinkVariant;
 } & Pick<NextLinkProps, 'href'> &
 	Pick<AnchorHTMLAttributes<HTMLAnchorElement>, 'children' | 'target'>;
@@ -13,12 +16,41 @@ export const Link = ({
 	href,
 	target,
 	children,
+	...props
 }: LinksProps) => {
+	const router = useRouter();
+
+	const hasFastInternet =
+		!navigator.connection || navigator.connection.effectiveType === '4g';
+
+	const prefetch = () => {
+		if (hasFastInternet) {
+			void router.prefetch(href.toString());
+		}
+	};
+
 	return (
 		<NextLink
 			href={href}
 			target={target}
 			rel={variant === 'external' ? 'noopener noreferrer' : undefined}
+			prefetch={false}
+			onMouseEnter={(e) => {
+				prefetch();
+				props.onMouseEnter?.(e);
+			}}
+			onPointerEnter={(e) => {
+				prefetch();
+				props.onPointerEnter?.(e);
+			}}
+			onTouchStart={(e) => {
+				prefetch();
+				props.onTouchStart?.(e);
+			}}
+			onFocus={(e) => {
+				prefetch();
+				props.onFocus?.(e);
+			}}
 		>
 			{children}
 		</NextLink>
