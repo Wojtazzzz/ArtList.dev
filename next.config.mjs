f6e8449640a2d38cc06ba41a0c1cd67e31ea51e9
Wojtazzzz <marcin.witas72@gmail.com>
2025-08-31 14:23:10 +0200

    build: add remote pattern to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'artlistapi.bieda.it',
+				port: '',
+				pathname: '/servers/**',
+				search: '',
+			},
+		],
+	},
+};
 
 export default nextConfig;
